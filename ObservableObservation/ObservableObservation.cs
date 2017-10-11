0b314ce1d363839aa409c63bd04a460855f2300d
Anders Schau Knatten <anders@knatten.org>
2017-10-11 09:31:52 +0200

    Simple function for printf debugging observables

---
+++ ObservableObservation/ObservableObservation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Reactive.Disposables;
+
+namespace org.knatten.ObservableObservation
+{
+	public static class ObservableObservation
+	{
+		private static int _length;
+		public static TextWriter Out;
+
+		static ObservableObservation()
+		{
+			Reset();	
+		}
+
+		public static IDisposable Write<T>(this IObservable<T> obs, string name = "<anon>")
+		{
+			if (name.Length > _length) _length = name.Length;
+
+			var subscription = obs.Subscribe(
+				v => Out.WriteLine(Preamble + "'{1}'", name, v),
+				e => Out.WriteLine(Preamble + "Error '{1}'", name, e),
+				() => Out.WriteLine(Preamble + "Completed", name)
+			);
+
+			return Disposable.Create(() =>
+			{
+				subscription.Dispose();
+				Out.WriteLine(Preamble + "Disposed", name);
+			});
+		}
+
+		private static string Preamble
+		{
+			get { return "{0,-" + _length + "}: "; }
+		}
+
+		public static void Reset()
+		{
+			_length = 0;
+			Out = Console.Out;
+		}
+	}
+}
---
+++ Tests/Tests.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.IO;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Text.RegularExpressions;
+using NUnit.Framework;
+using org.knatten.ObservableObservation;
+
+namespace Tests
+{
+    public class Tests
+    {
+	    private StringWriter _stringWriter;
+
+	    private string[] Out
+	    {
+		    get { return Regex.Split(_stringWriter.GetStringBuilder().ToString(), @"\r\n|\r|\n"); }
+	    }
+
+	    [SetUp]
+	    public void SetUp()
+	    {
+			_stringWriter = new StringWriter();
+			ObservableObservation.Reset();
+		    ObservableObservation.Out = _stringWriter;
+	    }
+
+	    [Test]
+	    public void WritesValuesAndCompletion()
+	    {
+		    var s = new Subject<int>();
+		    s.Write("n");
+			s.OnNext(1);
+			s.OnNext(2);
+			s.OnCompleted();
+			Assert.AreEqual("n: '1'", Out[0]);
+			Assert.AreEqual("n: '2'", Out[1]);
+			Assert.AreEqual("n: Completed", Out[2]);
+	    }
+
+	    [Test]
+	    public void WritesValuesAndError()
+	    {
+		    var s = new Subject<int>();
+		    s.Write("e");
+			s.OnNext(1);
+		    var exception = new Exception("Failed!");
+		    s.OnError(exception);
+			Assert.AreEqual("e: '1'", Out[0]);
+			Assert.AreEqual("e: Error '" + exception + "'", Out[1]);
+	    }
+
+	    [Test]
+	    public void AdjustsLength()
+	    {
+		    var hi = new Subject<int>();
+		    var hello = new Subject<int>();
+		    hi.Write("hi");
+			hi.OnNext(1);
+		    hello.Write("hello");
+			hi.OnNext(2);
+			hello.OnNext(1);
+			Assert.AreEqual("hi: '1'", Out[0]);
+			Assert.AreEqual("hi   : '2'", Out[1]);
+			Assert.AreEqual("hello: '1'", Out[2]);
+	    }
+
+		//TODO test that it disposes it's observable
+
+	    [Test]
+	    public void WritesDisposed()
+	    {
+		    var s = new Subject<int>();
+		    using (s.Write("hi"))
+		    {
+			    
+		    }
+			Assert.AreEqual("hi: Disposed", Out[0]);
+	    }
+
+	    [Test]
+	    public void Anonymous()
+	    {
+		    var s = new Subject<int>();
+		    s.Write();
+			s.OnCompleted();
+			Assert.AreEqual("<anon>: Completed", Out[0]);
+	    }
+
+	    [Test]
+	    public void DisposesWhatItObserves()
+	    {
+		    var disposed = false;
+		    var s = Observable.Create<int>(obs =>
+		    {
+			    return () => { disposed = true; };
+		    });
+		    using (s.Write())
+		    {
+			    
+		    }
+			Assert.True(disposed);
+	    }
+    }
+}
