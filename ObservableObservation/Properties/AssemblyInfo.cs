69377a713abf6fb22598aa78dc15ef23111249ff
Anders Schau Knatten <anders@knatten.org>
2017-10-11 09:48:36 +0200

    Made nuget package

--- ObservableObservation/Properties/AssemblyInfo.cs
+++ ObservableObservation/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ObservableObservation")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Simple library for printf debugging observables")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Anders Schau Knatten")]
 [assembly: AssemblyProduct("ObservableObservation")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
